831447d9eafffda000c2bf63aef60089360d5ee1
goutham0829 <123254334+goutham0829@users.noreply.github.com>
2025-01-22 09:29:47 +0530

    Add files via upload

--- t11.js
+++ t11.js
@@ -1,3 +1,11 @@
 function createProfile({name,email}){
     return {name,email};
 }
+const user={
+    name:"John doe",
+    email:"johndoe@example.com",
+    age:30,
+    address:'123 main St spring field'
+};
+const profile=createProfile(user);
+console.log(profile);
